a3bf8baef9ff64e37d5e57ef6efd86a1cc6e7f9b
OierGman <75170798+OierGman@users.noreply.github.com>
2022-02-17 17:40:10 +0000

    testing the UI

--- MorseRSAAlgorithms/MainForm.cs
+++ MorseRSAAlgorithms/MainForm.cs
@@ -29,9 +29,9 @@ namespace MorseRSAAlgorithms
             InitializeComponent();
         }
 
-        private void MainForm_Load(object sender, EventArgs e)
+        private void button1_Click(object sender, EventArgs e)
         {
-
+            MessageBox.Show("This is a test");
         }
     }
 }
